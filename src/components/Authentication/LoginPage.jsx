8157530f32138ab158a4114aa4530cf37de324a8
JB <drv98@naver.com>
2023-12-19 15:00:54 +0900

    유효성 검사

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -2,7 +2,11 @@ import { useForm } from 'react-hook-form';
 import './LoginPage.css';
 
 const LoginPage = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const submitData = (formData) => console.log(formData);
   return (
     <section className="align_center form_page">
@@ -12,20 +16,25 @@ const LoginPage = () => {
           <div>
             <label htmlFor="email">Email</label>
             <input
-              {...register('email')}
+              {...register('email', { required: '이메일을 입력해주세요.' })}
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
             />
+            {errors.email && <em className="form_error">{errors.email.message}</em>}
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
-              {...register('password')}
+              {...register('password', {
+                required: '패스워드를 입력해주세요.',
+                minLength: { value: 4, message: '패스워드는 최소 4자 이상.' },
+              })}
               id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
+            {errors.password && <em className="form_error">{errors.password.message}</em>}
           </div>
 
           <button type="submit" className="search_button form_submit">
