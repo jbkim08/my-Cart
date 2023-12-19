0da1ab9aba4139b501bbc3cf5bc2b34ac7d56f2a
JB <drv98@naver.com>
2023-12-19 13:55:35 +0900

    장바구니 페이지

---
+++ src/components/Common/Table.jsx
@@ -0,0 +1,20 @@
+import './Table.css';
+
+// Table컴포넌트는 열의 제목들이 배열로 입력되면 제목들을 th태그로 만듬
+// children 자식태그들을 표시함.
+const Table = ({ headings, children }) => {
+  return (
+    <table className="common_table">
+      <thead>
+        <tr>
+          {headings.map((item, index) => (
+            <th key={index}>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      {children}
+    </table>
+  );
+};
+
+export default Table;
