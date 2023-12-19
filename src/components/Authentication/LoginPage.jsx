822254d658a92279d9e5c66dd1301a700e4f7fd2
JB <drv98@naver.com>
2023-12-19 14:44:36 +0900

    react hook form 사용

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,29 +1,18 @@
-import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './LoginPage.css';
 
 const LoginPage = () => {
-  //리액트에서 특정 태그를 선택하는 방법
-  const passwordRef = useRef(null);
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(user);
-    setUser({ email: '', password: '' });
-  };
+  const { register, handleSubmit } = useForm();
+  const submitData = (formData) => console.log(formData);
   return (
     <section className="align_center form_page">
-      <form onSubmit={handleSubmit} className="authentication_form">
+      <form onSubmit={handleSubmit(submitData)} className="authentication_form">
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              value={user.email}
-              type="email"
+              {...register('email')}
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
@@ -32,20 +21,11 @@ const LoginPage = () => {
           <div>
             <label htmlFor="password">Password</label>
             <input
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              value={user.password}
-              type="password"
-              ref={passwordRef}
+              {...register('password')}
               id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
-            <button type="button" onClick={() => (passwordRef.current.type = 'password')}>
-              비밀번호 숨기기
-            </button>
-            <button type="button" onClick={() => (passwordRef.current.type = 'text')}>
-              비밀번호 보이게
-            </button>
           </div>
 
           <button type="submit" className="search_button form_submit">
