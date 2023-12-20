57f8557a54bdd5ff969400d04dfc4a64b4b9c09e
JB <drv98@naver.com>
2023-12-20 11:29:05 +0900

    로딩시 스켈레톤 보이기

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -4,7 +4,7 @@ import useData from '../../Hook/useData';
 import ProductCardSkeleton from './ProductCardSkeleton';
 
 const ProductsList = () => {
-  const { data, error } = useData('/products');
+  const { data, error, isLoading } = useData('/products');
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <section className="products_list_section">
@@ -21,9 +21,7 @@ const ProductsList = () => {
 
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {skeletons.map((n) => (
-          <ProductCardSkeleton key={n} />
-        ))}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
         {data.products &&
           data.products.map((product) => (
             <ProductCard
