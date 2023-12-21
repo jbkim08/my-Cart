1b53f16e447a552c72cd9ede93ccc2b718285ad6
JB <drv98@naver.com>
2023-12-21 10:39:11 +0900

    로그인 에러및 수정

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,7 +1,8 @@
-import { useForm } from 'react-hook-form';
-import './LoginPage.css';
-import { login } from '../../services/userServices';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { login } from '../../services/userServices';
+import './LoginPage.css';
 
 const LoginPage = () => {
   const [formError, setFormError] = useState('');
@@ -14,35 +15,40 @@ const LoginPage = () => {
   const submitData = async (formData) => {
     try {
       await login(formData);
-    } catch (error) {
-      setFormError(error.response.data.message);
+
+      window.location = '/';
+    } catch (err) {
+      setFormError(err.response.data.message);
     }
   };
+
   return (
     <section className="align_center form_page">
-      <form onSubmit={handleSubmit(submitData)} className="authentication_form">
+      <form className="authentication_form" onSubmit={handleSubmit(submitData)}>
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
-              {...register('email', { required: '이메일을 입력해주세요.' })}
+              type="email"
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
+              {...register('email', { required: '이메일을 이력해주세요.' })}
             />
             {errors.email && <em className="form_error">{errors.email.message}</em>}
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
-              {...register('password', {
-                required: '패스워드를 입력해주세요.',
-                minLength: { value: 4, message: '패스워드는 최소 4자 이상.' },
-              })}
+              type="password"
               id="password"
               className="form_text_input"
-              placeholder="패스워드"
+              placeholder="패스워드 입력..."
+              {...register('password', {
+                required: '패스워드를 입력해주세요.',
+                minLength: { value: 8, message: '패스워드는 최소 8자 이상.' },
+              })}
             />
             {errors.password && <em className="form_error">{errors.password.message}</em>}
           </div>
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,7 @@
 import apiClient from '../utils/api-client';
 
 export async function signup(user, profile) {
+  //이미지도 함께 폼데이터 객체에 담아서 서버로 제출
   const body = new FormData();
   body.append('name', user.name);
   body.append('email', user.email);
@@ -8,13 +9,11 @@ export async function signup(user, profile) {
   body.append('deliveryAddress', user.deliveryAddress);
   body.append('profilePic', profile);
 
-  await apiClient.post('/user/signup', body);
+  const { data } = await apiClient.post('/user/signup', body);
+  localStorage.setItem('token', data.token);
 }
 
 export async function login(user) {
-  const body = new FormData();
-  body.append('email', user.email);
-  body.append('password', user.password);
-
-  await apiClient.post('/user/login', body);
+  const { data } = await apiClient.post('/user/login', user);
+  localStorage.setItem('token', data.token);
 }
