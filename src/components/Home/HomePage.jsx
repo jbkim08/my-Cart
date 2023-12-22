87774e595db7ca3dd0bc3bdb4878bde6385f1008
JB <drv98@naver.com>
2023-12-22 11:20:37 +0900

    홈페이지 바로구매 링크

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       <HeroSection
         title="아이폰 14 프로 그 이상"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
-        link="/"
+        link="/product/658234e35a7bb96efdb7a1ef"
         image={iphone}
       />
 
@@ -19,7 +19,7 @@ const HomePage = () => {
       <HeroSection
         title="궁극의 장비를 세팅하세요"
         subtitle="You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini."
-        link="/"
+        link="/product/658234e35a7bb96efdb7a1f7"
         image={mac}
       />
     </div>
